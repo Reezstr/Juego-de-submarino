5d692ec86180933e351f49ac347cbf63602e9f77
Ian rojas <iansamuel.rojas@gmail.com>
2026-04-19 22:09:48 -0500

    Movimiento fisico
    Se cambio el como funciona el movimiento del personaje principal, para que en lugar de moverse transformando el componente posicion, se mueve haciendo uso de la fisica de un rigid body

--- Assets/Codigo/Controlador.cs
+++ Assets/Codigo/Controlador.cs
@@ -3,34 +3,52 @@ using UnityEngine;
 public class Controlador : MonoBehaviour
 {
     int rotacion = 0;
-    int velocidad = 5;
+    int velocidad = 1;
     bool avanzando = true;
     bool rotando = true;
+    public float fuerzaMovimiento = 10f;
+    public float fuerzaMovimientoAtras = 4f;
+    public Rigidbody submarino;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         avanzando = false;
+        rotando = false;
     }
 
     // Update is called once per frame
     void Update()
-    {
+        {
+
+        Debug.Log("rotando =" + rotando);
+        Debug.Log("rotando =" + avanzando);
+        
         if (avanzando == false && Input.GetKey("left")){
-            rotacion += 2;
             rotando = true;
+            rotacion += 2;
         }
         if (avanzando == false && Input.GetKey("right")){
-            rotacion = rotacion - 2;
             rotando = true;
+            rotacion = rotacion - 1;
         }
 
 
-        
-        float moveVertical = Input.GetAxis("Vertical");
 
-        Vector3 direction = new Vector3(0, moveVertical, 0);
-        transform.Translate(direction * velocidad * Time.deltaTime);
+
+        if (Input.GetKey("up")) {
+        submarino.linearVelocity = transform.up * fuerzaMovimiento;
+        }
+        if (Input.GetKey("down")) {
+        submarino.linearVelocity = -transform.up * fuerzaMovimientoAtras;
+        }
+
+        if (submarino.linearVelocity.magnitude < 0.1f) {
+        submarino.linearVelocity = Vector3.zero;
+        submarino.angularVelocity = Vector3.zero; // Detiene también micro-rotaciones
+        }
+            
+        
         transform.rotation = Quaternion.Euler(0, 0, rotacion);
     }
 }
